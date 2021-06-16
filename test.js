796af7ee2f126f4c99ba8e53d423e560cd54b432
Neil-140301 <alvares.1@iitj.ac.in>
2021-06-16 17:59:57 +0530

    commit 11

--- test.js
+++ test.js
@@ -1,14 +1,30 @@
 alert('This is neil Alvares');
 
 let myDiv = document.createElement('div');
-myDiv.classList.add('mydiv');
+myDiv.style.backgroundColor = 'purple';
+myDiv.style.position = 'fixed';
+myDiv.style.right = '20px';
+myDiv.style.bottom = '100px';
+myDiv.style.width = '300px';
+myDiv.style.height = '250px';
+myDiv.style.borderRadius = '5px';
 
 let btn = document.createElement('button');
-btn.classList.add('mybtn');
+btn.style.backgroundColor = 'purple';
+btn.style.position = 'fixed';
+btn.style.right = '20px';
+btn.style.bottom = '10px';
+btn.style.padding = '30px';
+btn.style.borderRadius = '50%';
+
 btn.textContent = 'this is a button';
 btn.addEventListener('click', () => {
-  myDiv.classList.toggle('d-none');
+  if (myDiv.style.display === 'none') {
+    myDiv.style.display = 'block';
+  } else {
+    myDiv.style.display = 'none';
+  }
   console.log('myDiv clicked');
-})
+});
 
 document.body.appendChild(btn);
