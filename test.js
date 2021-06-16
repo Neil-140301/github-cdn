bb3a9c00e706d1e6ae21ff1ad829d6bbd0e07c3a
Neil-140301 <alvares.1@iitj.ac.in>
2021-06-16 13:22:01 +0530

    commit 8

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ let myDiv = document.getElementById('myDiv');
 
 let btn = document.createElement('button');
 btn.classList.add('mybtn');
+btn.textContent = 'this is a button';
 btn.addEventListener('click', () => {
   myDiv.classList.toggle('d-none');
   console.log('myDiv clicked');
