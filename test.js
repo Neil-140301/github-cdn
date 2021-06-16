9ba5a2f118422064099faad1b512f13494f532d1
Neil-140301 <alvares.1@iitj.ac.in>
2021-06-16 13:19:46 +0530

    commit 7

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ let btn = document.createElement('button');
 btn.classList.add('mybtn');
 btn.addEventListener('click', () => {
   myDiv.classList.toggle('d-none');
+  console.log('myDiv clicked');
 })
 
 document.getElementById('root').appendChild(btn);
