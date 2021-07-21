96149a64a991b5e34ac3afdde65512b8b6a97954
Neil Alvares <74774395+Neil-140301@users.noreply.github.com>
2021-07-21 21:00:17 +0530

    Update pop_script.js

--- pop_script.js
+++ pop_script.js
@@ -2,7 +2,8 @@ console.log('Superassistant script loaded');
 
 const SA_APP_URL = 'https://81a35f753a15.ngrok.io';
 
-const SA_Shop_Domain = 'super-pops-test.myshopify.com';
+const SA_Shop_Domain = Shopify.shop;
+// const SA_Shop_Domain = 'super-pops-test.myshopify.com';
 
 const LIVE_VISIT_DELAY = 0.1; //in hours (6 min)
 const RECENT_VISIT_DELAY = 24; //in hours
@@ -88,19 +89,57 @@ const checkCart = async function () {
     const response = await fetch(`https://${SA_Shop_Domain}/cart.js`);
     const data = await response.json();
     const location = localStorage.getItem('superAssistant-ipInfo');
+    let customer = {};
+
     if (data.items.length > 0) {
-      let payload = {
-        shopDomain: SA_Shop_Domain,
-        city: location.city,
-        province: location.state,
-        country: location.country_name,
-        customer: {id: 123}
+      //check for customer
+      if (meta.page.customerId) {
+        customer = { id: meta.page.customerId, tag: 'shopify' };
+      } else {
+        let newId = localStorage.getItem('SA_customer_temp_id')
+          ? localStorage.getItem('SA_customer_temp_id')
+          : Date.now();
+        localStorage.setItem('SA_customer_temp_id', newId);
+        customer = { id: newId, tag: 'temporary' };
+      }
+
+      let product = data.items[0];
+
+      if (product.id !== localStorage.getItem('SA_last_product_added')) {
+        console.log('sending data...');
+        let payload = {
+          shopDomain: SA_Shop_Domain,
+          location: {
+            city: location.city,
+            province: location.state,
+            country: location.country_name,
+          },
+          customer: customer,
+          product: {
+            id: product.id,
+            title: product.title,
+            added_at: new Date(),
+          },
+        };
+
+        const options = {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify(payload),
+        };
+
+        await fetch(`${SA_APP_URL}/api/popData/add-to-carts`, options);
+
+        localStorage.setItem('SA_last_product_added', product.id);
       }
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 let leave = new Date(localStorage.getItem('SuperAssistant-leaveTimestamp'));
 let current = new Date();
@@ -134,7 +173,25 @@ window.onbeforeunload = () => {
   localStorage.setItem('SuperAssistant-leaveTimestamp', new Date());
 };
 
-getLiveVisits();
-getRecentVisits();
+setInterval(checkCart, 2000);
 
-checkCart()
+if (localStorage.getItem('SA_customer_temp_id') && meta.page.customerId) {
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      shopDomain: SA_Shop_Domain,
+      tempId: localStorage.getItem('SA_customer_temp_id'),
+      shopifyId: meta.page.customerId,
+    }),
+  };
+
+  fetch(`${SA_APP_URL}/api/popData/update-ids`, options)
+    .then(() => console.log('success'))
+    .catch((err) => console.log(err));
+
+  localStorage.removeItem('SA_customer_temp_id');
+}
