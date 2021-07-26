a3cac23e3e93b741e7f2f309cc86f687b1770857
Neil Alvares <74774395+Neil-140301@users.noreply.github.com>
2021-07-26 16:24:23 +0530

    Update pop_script.js

--- pop_script.js
+++ pop_script.js
@@ -1,4 +1,6 @@
 console.log('Superassistant script loaded');
+/* Backend Url */
+const SA_APP_URL = 'https://293b1e37a243.ngrok.io';
 
 /*fontawesome script*/
 let SA_script = document.createElement('script');
@@ -38,16 +40,11 @@ SA_link.href =
 SA_link.rel = 'stylesheet';
 document.head.appendChild(SA_link);
 
-
-/* Backend Url */
-const SA_APP_URL = 'https://293b1e37a243.ngrok.io';
-
 const SA_Shop_Domain = Shopify.shop;
 // const SA_Shop_Domain = 'super-pops-test.myshopify.com';
 
 const LIVE_VISIT_DELAY = 0.1; //in hours (6 min)
 const RECENT_VISIT_DELAY = 24; //in hours
-
 
 const newLiveVisit = async function (time) {
   console.log('new live visit at ' + time);
@@ -110,6 +107,43 @@ const getRecentVisits = async function () {
     const data = await response.json();
     console.log(data);
     return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getOrderPops = async function () {
+  try {
+    const response = await fetch(
+      `${SA_APP_URL}/api/popData/orders/${SA_Shop_Domain}`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAddToCartPops = async function () {
+  try {
+    const response = await fetch(
+      `${SA_APP_URL}/api/popData/add-to-carts/${SA_Shop_Domain}`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getLatestPopData = async function () {
+  try {
+    const response = await fetch(`${SA_APP_URL}/api/popData/${SA_Shop_Domain}`);
+    const data = await response.json();
+    console.log(data);
+    localStorage.setItem('SA_latest_popData', JSON.stringify(data));
   } catch (error) {
     console.log(error);
   }
@@ -184,6 +218,119 @@ const getMerchantDetails = async function () {
     return data;
   } catch (error) {
     console.log(error);
+  }
+};
+
+/* */
+const firstTierPop = function (merchant, liveVisits, recentVisits) {
+  const { lastHour, last6Hour, last24Hour, last48Hour, lastWeek } = JSON.parse(
+    localStorage.getItem('SA_latest_popData')
+  );
+
+  console.log('first tier pop');
+
+  let orderPop =
+    lastHour.orders.pop() ||
+    last6Hour.orders.pop() ||
+    last24Hour.orders.pop() ||
+    last48Hour.orders.pop() ||
+    lastWeek.orders.pop();
+
+  if (orderPop) {
+    localStorage.setItem(
+      'SA_latest_popData',
+      JSON.stringify({ lastHour, last6Hour, last24Hour, last48Hour, lastWeek })
+    );
+    return createOrdersPop(
+      orderPop.firstName,
+      orderPop.location,
+      orderPop.order.products[0].title,
+      merchant.orders
+    );
+  }
+
+  let addToCartsPop =
+    lastHour.addToCarts.pop() ||
+    last6Hour.addToCarts.pop() ||
+    last24Hour.addToCarts.pop() ||
+    last48Hour.addToCarts.pop() ||
+    lastWeek.addToCarts.pop();
+
+  if (addToCartsPop) {
+    localStorage.setItem(
+      'SA_latest_popData',
+      JSON.stringify({ lastHour, last6Hour, last24Hour, last48Hour, lastWeek })
+    );
+    return createAddToCartsPop(
+      addToCartsPop.firstName,
+      addToCartsPop.location,
+      addToCartsPop.product.title,
+      merchant.addToCarts
+    );
+  }
+
+  if (liveVisits > 0) {
+    return createLiveVisitorPop(liveVisits, merchant.liveVisitors);
+  }
+
+  if (recentVisits > 0) {
+    return createRecentVisitorPop(recentVisits, merchant.recentVisitors);
+  }
+};
+
+const secondTierPop = function (merchant, liveVisits, recentVisits) {
+  const { lastHour, last6Hour, last24Hour, last48Hour, lastWeek } = JSON.parse(
+    localStorage.getItem('SA_latest_popData')
+  );
+
+  console.log('second tier pop');
+
+  if (liveVisits > 0) {
+    return createLiveVisitorPop(liveVisits, merchant.liveVisitors);
+  }
+
+  let addToCartsPop =
+    lastHour.addToCarts.pop() ||
+    last6Hour.addToCarts.pop() ||
+    last24Hour.addToCarts.pop() ||
+    last48Hour.addToCarts.pop() ||
+    lastWeek.addToCarts.pop();
+
+  if (addToCartsPop) {
+    localStorage.setItem(
+      'SA_latest_popData',
+      JSON.stringify({ lastHour, last6Hour, last24Hour, last48Hour, lastWeek })
+    );
+    return createAddToCartsPop(
+      addToCartsPop.firstName,
+      addToCartsPop.location,
+      addToCartsPop.product.title,
+      merchant.addToCarts
+    );
+  }
+
+  let orderPop =
+    lastHour.orders.pop() ||
+    last6Hour.orders.pop() ||
+    last24Hour.orders.pop() ||
+    last48Hour.orders.pop() ||
+    lastWeek.orders.pop();
+
+  if (orderPop) {
+    localStorage.setItem(
+      'SA_latest_popData',
+      JSON.stringify({ lastHour, last6Hour, last24Hour, last48Hour, lastWeek })
+    );
+    return createOrdersPop(
+      orderPop.firstName,
+      orderPop.location,
+      orderPop.order.products[0].title,
+      merchant.orders
+    );
+  }
+
+  if (recentVisits > 0) {
+    return createRecentVisitorPop(recentVisits, merchant.recentVisitors);
   }
 };
 
@@ -460,23 +607,60 @@ const createOrdersPop = function (firstName, location, productTitle, settings) {
 
 /* main function */
 const renderSAPops = async function () {
-  const { count } = await getRecentVisits();
+  const liveVisits = await getLiveVisits();
+  const recentVisits = await getRecentVisits();
   const merchant = await getMerchantDetails();
+  await getLatestPopData();
 
-  $(function () {
-    $('#neil').fadeIn('slow', function () {
-      console.log('hi faded in');
-    });
-  });
+  /*Pop container */
+  let SA_pop_container = document.createElement('div');
+  SA_pop_container.style.cssText += `border: #e5e5e5 solid 1px;
+  height: 100px;
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  font-family: 'Roboto';
+  display: none;`;
+  SA_pop_container.id = 'SA_pop_container';
+  document.body.appendChild(SA_pop_container);
 
-  let newPop = createAddToCartsPop(
-    'Neil',
-    { city: 'Pune', province: 'Maharashtra', country: 'India' },
-    'Apple Iphone 12 Pro',
-    merchant.addToCarts
-  );
+  let newPop;
+  const pops_per_session = 5;
+  const firstTier = 2;
+  const secondTier = 2;
+  let popCount = 0;
 
-  document.getElementById('neil').appendChild(newPop);
+  let intervalId = setInterval(function () {
+    popCount++;
+    console.log(popCount);
+
+    $('#SA_pop_container').empty();
+
+    if (popCount <= firstTier) {
+      newPop = firstTierPop(merchant, liveVisits.count, recentVisits.count);
+    }
+
+    if (firstTier < popCount && popCount <= firstTier + secondTier) {
+      newPop = secondTierPop(merchant, liveVisits.count, recentVisits.count);
+    }
+
+    if (firstTier + secondTier < popCount && popCount <= pops_per_session) {
+      newPop = firstTierPop(merchant, liveVisits.count, recentVisits.count);
+    }
+
+    document.getElementById('SA_pop_container').appendChild(newPop);
+    $('#SA_pop_container').fadeIn('slow');
+    setTimeout(function () {
+      $('#SA_pop_container').fadeOut('slow');
+    }, 4000);
+
+    if (popCount === pops_per_session) {
+      clearInterval(intervalId);
+      setTimeout(function () {
+        $('#SA_pop_container').fadeOut('slow');
+      }, 8000);
+    }
+  }, 8000);
 };
 
 renderSAPops();
